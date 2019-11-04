bf1c50b1957ca19f49c29bfd614ca0418ef82f1f
nyaasu <nyaasu@qq.com>
2019-11-04 20:02:55 +0800

    :sparkles: feat/替换内容可自定义

--- zhangyizhu/2/index.js
+++ zhangyizhu/2/index.js
@@ -1,20 +1,21 @@
 
 function replace() {
-  var value = document.getElementsByTagName('input')[0].value
-  if (value !="") {
+  var find_value = document.getElementsByTagName('input')[0].value
+  var replace_value = document.getElementsByTagName('input')[1].value
+  if (find_value !="" || replace_value !="") {
     while(true) {
       var content_1 = document.getElementById('content').innerText
       var arr_content_1 = content_1.split('')
-      var word_length = value.length  //2
-      var word_index = content_1.indexOf(value)
+      var word_length = find_value.length  //2
+      var word_index = content_1.indexOf(find_value)
       if (word_index == -1) {
         break
       }
-      arr_content_1.splice(word_index,word_length,'大哥')
+      arr_content_1.splice(word_index,word_length,replace_value)
       document.getElementById('content').innerText = arr_content_1.join('')
     }
   }
   else {
-    alert('请输入查找内容！')
+    alert('请输入查找或替换内容！')
   }
 }
